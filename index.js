d7e578263ead990fefa756871d508b8f499c66ac
Patrick Cavanagh <pdcavanagh@yahoo.com>
2017-02-27 23:25:06 -0500

    updated automatic port assignment

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var CryptoJS = require("crypto-js");
 
-var PORT = 3000;
+var port = process.env.PORT;
+if(!port) {
+  port=3000;
+}
 
 var app = express();
 
